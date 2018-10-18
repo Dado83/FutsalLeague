fb0c48d013c5e29c4b855a206ccfbe746c32c2ad
Dado83 <83deadpool@gmail.com>
2018-10-18 16:13:19 +0200

    add multiple html forms for league data input

--- src/main/java/dp/futsal/FutsalController.java
+++ src/main/java/dp/futsal/FutsalController.java
@@ -57,17 +57,52 @@ public class FutsalController {
         return "adminDashboard";
     }
 
-    @GetMapping("/editTeam")
-    public String editTeam(Model model, int index) {
+    
+    
+    @GetMapping("/editTeam5")
+    public String editTeam5(Model model, int index) {
         model.addAttribute("team", service.getTeamListIDindexed5().get(index));
         model.addAttribute("results", service.getTeamListIDindexed5().get(index).getResults());
         model.addAttribute("leagueTable", service.getTeamListSorted5());
         model.addAttribute("pairs", service.getPairs());
-        return "editTeam";
+        return "editTeam5";
     }
+    @GetMapping("/editTeam6")
+    public String editTeam6(Model model, int index) {
+        model.addAttribute("team", service.getTeamListIDindexed6().get(index));
+        model.addAttribute("results", service.getTeamListIDindexed6().get(index).getResults());
+        model.addAttribute("leagueTable", service.getTeamListSorted6());
+        model.addAttribute("pairs", service.getPairs());
+        return "editTeam6";
+    }
+    @GetMapping("/editTeam7")
+    public String editTeam7(Model model, int index) {
+        model.addAttribute("team", service.getTeamListIDindexed7().get(index));
+        model.addAttribute("results", service.getTeamListIDindexed7().get(index).getResults());
+        model.addAttribute("leagueTable", service.getTeamListSorted7());
+        model.addAttribute("pairs", service.getPairs());
+        return "editTeam7";
+    }
+    @GetMapping("/editTeam8")
+    public String editTeam8(Model model, int index) {
+        model.addAttribute("team", service.getTeamListIDindexed8().get(index));
+        model.addAttribute("results", service.getTeamListIDindexed8().get(index).getResults());
+        model.addAttribute("leagueTable", service.getTeamListSorted8());
+        model.addAttribute("pairs", service.getPairs());
+        return "editTeam8";
+    }
+    @GetMapping("/editTeam9")
+    public String editTeam9(Model model, int index) {
+        model.addAttribute("team", service.getTeamListIDindexed9().get(index));
+        model.addAttribute("results", service.getTeamListIDindexed9().get(index).getResults());
+        model.addAttribute("leagueTable", service.getTeamListSorted9());
+        model.addAttribute("pairs", service.getPairs());
+        return "editTeam9";
+    }
+    
 
-    @PostMapping("/updatedTeam")
-    public String updateTeam(Model model, @ModelAttribute Team team) {
+    @PostMapping("/updatedTeam5")
+    public String updateTeam5(Model model, @ModelAttribute Team team) {
         LOGGER.info("" + team.getTeamName());
 
         Team teamTemp = service.getTeam5(team.getId());
@@ -83,19 +118,125 @@ public class FutsalController {
         return "updatedTeam";
     }
 
-    @GetMapping("/enterMatchDayResults")
-    public String enterResults(Model model, int index) {
+    @PostMapping("/updatedTeam6")
+    public String updateTeam6(Model model, @ModelAttribute Team team) {
+        LOGGER.info("" + team.getTeamName());
+
+        Team teamTemp = service.getTeam6(team.getId());
+        List<MatchResult> matchResultList = teamTemp.getResults();
+        team.setResults(matchResultList);
+
+        service.getTeamMap6().put(team.getId(), team);
+        service.updateTeamData6(service.getTeamMap6());
+        model.addAttribute("updatedTeam", team);
+        model.addAttribute("leagueTable", service.getTeamListSorted6());
+        model.addAttribute("pairs", service.getPairs());
+        LOGGER.info(service.getTeamMap6().toString());
+        return "updatedTeam";
+    }
+    @PostMapping("/updatedTeam7")
+    public String updateTeam7(Model model, @ModelAttribute Team team) {
+        LOGGER.info("" + team.getTeamName());
+
+        Team teamTemp = service.getTeam7(team.getId());
+        List<MatchResult> matchResultList = teamTemp.getResults();
+        team.setResults(matchResultList);
+
+        service.getTeamMap7().put(team.getId(), team);
+        service.updateTeamData7(service.getTeamMap7());
+        model.addAttribute("updatedTeam", team);
+        model.addAttribute("leagueTable", service.getTeamListSorted7());
+        model.addAttribute("pairs", service.getPairs());
+        LOGGER.info(service.getTeamMap7().toString());
+        return "updatedTeam";
+    }
+    @PostMapping("/updatedTeam8")
+    public String updateTeam8(Model model, @ModelAttribute Team team) {
+        LOGGER.info("" + team.getTeamName());
+
+        Team teamTemp = service.getTeam8(team.getId());
+        List<MatchResult> matchResultList = teamTemp.getResults();
+        team.setResults(matchResultList);
+
+        service.getTeamMap8().put(team.getId(), team);
+        service.updateTeamData8(service.getTeamMap8());
+        model.addAttribute("updatedTeam", team);
+        model.addAttribute("leagueTable", service.getTeamListSorted8());
+        model.addAttribute("pairs", service.getPairs());
+        LOGGER.info(service.getTeamMap8().toString());
+        return "updatedTeam";
+    }
+    @PostMapping("/updatedTeam9")
+    public String updateTeam9(Model model, @ModelAttribute Team team) {
+        LOGGER.info("" + team.getTeamName());
+
+        Team teamTemp = service.getTeam9(team.getId());
+        List<MatchResult> matchResultList = teamTemp.getResults();
+        team.setResults(matchResultList);
+
+        service.getTeamMap9().put(team.getId(), team);
+        service.updateTeamData9(service.getTeamMap9());
+        model.addAttribute("updatedTeam", team);
+        model.addAttribute("leagueTable", service.getTeamListSorted9());
+        model.addAttribute("pairs", service.getPairs());
+        LOGGER.info(service.getTeamMap9().toString());
+        return "updatedTeam";
+    }
+    
+    
+    @GetMapping("/enterMatchDayResults5")
+    public String enterResults5(Model model, int index) {
         MatchDayForm mDayForm = new MatchDayForm();
         mDayForm.loadForm(index, service.getTeamListIDindexed5(), service.getPairs());
         model.addAttribute("gameForm", mDayForm);
         model.addAttribute("pairs", service.getPairs());
         model.addAttribute("leagueTable", service.getTeamListSorted5());
         model.addAttribute("results", service.getResults5().get(index));
-        return "enterMatchDayResults";
+        return "enterMatchDayResults5";
+    }
+    @GetMapping("/enterMatchDayResults6")
+    public String enterResults6(Model model, int index) {
+        MatchDayForm mDayForm = new MatchDayForm();
+        mDayForm.loadForm(index, service.getTeamListIDindexed6(), service.getPairs());
+        model.addAttribute("gameForm", mDayForm);
+        model.addAttribute("pairs", service.getPairs());
+        model.addAttribute("leagueTable", service.getTeamListSorted6());
+        model.addAttribute("results", service.getResults6().get(index));
+        return "enterMatchDayResults6";
+    }
+    @GetMapping("/enterMatchDayResults7")
+    public String enterResults7(Model model, int index) {
+        MatchDayForm mDayForm = new MatchDayForm();
+        mDayForm.loadForm(index, service.getTeamListIDindexed7(), service.getPairs());
+        model.addAttribute("gameForm", mDayForm);
+        model.addAttribute("pairs", service.getPairs());
+        model.addAttribute("leagueTable", service.getTeamListSorted7());
+        model.addAttribute("results", service.getResults7().get(index));
+        return "enterMatchDayResults7";
+    }
+    @GetMapping("/enterMatchDayResults8")
+    public String enterResults8(Model model, int index) {
+        MatchDayForm mDayForm = new MatchDayForm();
+        mDayForm.loadForm(index, service.getTeamListIDindexed8(), service.getPairs());
+        model.addAttribute("gameForm", mDayForm);
+        model.addAttribute("pairs", service.getPairs());
+        model.addAttribute("leagueTable", service.getTeamListSorted8());
+        model.addAttribute("results", service.getResults8().get(index));
+        return "enterMatchDayResults8";
+    }
+    @GetMapping("/enterMatchDayResults9")
+    public String enterResults9(Model model, int index) {
+        MatchDayForm mDayForm = new MatchDayForm();
+        mDayForm.loadForm(index, service.getTeamListIDindexed9(), service.getPairs());
+        model.addAttribute("gameForm", mDayForm);
+        model.addAttribute("pairs", service.getPairs());
+        model.addAttribute("leagueTable", service.getTeamListSorted9());
+        model.addAttribute("results", service.getResults9().get(index));
+        return "enterMatchDayResults9";
     }
 
-    @PostMapping("/addedMatchDayResults")
-    public String addedResults(Model model, @ModelAttribute MatchDayForm form) {
+    @PostMapping("/addedMatchDayResults5")
+    public String addedResults5(Model model, @ModelAttribute MatchDayForm form) {
         LOGGER.info("pocetak addedmatch");
 
         form.setTeamMap(service.getTeamMap5());
@@ -115,6 +256,87 @@ public class FutsalController {
         return "addedResults";
     }
 
+    @PostMapping("/addedMatchDayResults6")
+    public String addedResults6(Model model, @ModelAttribute MatchDayForm form) {
+        LOGGER.info("pocetak addedmatch");
+
+        form.setTeamMap(service.getTeamMap6());
+
+        for (int i = 1; i < 11; i++) {
+            service.getTeam6(i).delMatchDay(form.getmDay() + "");
+        }
+
+        form.saveResults(service.getResults6(), service.getGamePostponed6(), service.getNotPlaying6());
+        service.setTeamMap6(form.getTeamMap());
+        service.updateTeamData6(service.getTeamMap6());
+        model.addAttribute("result", form.getResults());
+        model.addAttribute("pairs", service.getPairs());
+        model.addAttribute("leagueTable", service.getTeamListSorted6());
+        LOGGER.info(service.getGamePostponed6().toString());
+        LOGGER.info(service.getNotPlaying6().toString());
+        return "addedResults";
+    }
+    @PostMapping("/addedMatchDayResults7")
+    public String addedResults7(Model model, @ModelAttribute MatchDayForm form) {
+        LOGGER.info("pocetak addedmatch");
+
+        form.setTeamMap(service.getTeamMap7());
+
+        for (int i = 1; i < 11; i++) {
+            service.getTeam7(i).delMatchDay(form.getmDay() + "");
+        }
+
+        form.saveResults(service.getResults7(), service.getGamePostponed7(), service.getNotPlaying7());
+        service.setTeamMap7(form.getTeamMap());
+        service.updateTeamData7(service.getTeamMap7());
+        model.addAttribute("result", form.getResults());
+        model.addAttribute("pairs", service.getPairs());
+        model.addAttribute("leagueTable", service.getTeamListSorted7());
+        LOGGER.info(service.getGamePostponed7().toString());
+        LOGGER.info(service.getNotPlaying7().toString());
+        return "addedResults";
+    }
+    @PostMapping("/addedMatchDayResults8")
+    public String addedResults8(Model model, @ModelAttribute MatchDayForm form) {
+        LOGGER.info("pocetak addedmatch");
+
+        form.setTeamMap(service.getTeamMap8());
+
+        for (int i = 1; i < 11; i++) {
+            service.getTeam8(i).delMatchDay(form.getmDay() + "");
+        }
+
+        form.saveResults(service.getResults8(), service.getGamePostponed8(), service.getNotPlaying8());
+        service.setTeamMap8(form.getTeamMap());
+        service.updateTeamData8(service.getTeamMap8());
+        model.addAttribute("result", form.getResults());
+        model.addAttribute("pairs", service.getPairs());
+        model.addAttribute("leagueTable", service.getTeamListSorted8());
+        LOGGER.info(service.getGamePostponed8().toString());
+        LOGGER.info(service.getNotPlaying8().toString());
+        return "addedResults";
+    }
+    @PostMapping("/addedMatchDayResults9")
+    public String addedResults9(Model model, @ModelAttribute MatchDayForm form) {
+        LOGGER.info("pocetak addedmatch");
+
+        form.setTeamMap(service.getTeamMap9());
+
+        for (int i = 1; i < 11; i++) {
+            service.getTeam9(i).delMatchDay(form.getmDay() + "");
+        }
+
+        form.saveResults(service.getResults9(), service.getGamePostponed9(), service.getNotPlaying9());
+        service.setTeamMap9(form.getTeamMap());
+        service.updateTeamData5(service.getTeamMap9());
+        model.addAttribute("result", form.getResults());
+        model.addAttribute("pairs", service.getPairs());
+        model.addAttribute("leagueTable", service.getTeamListSorted9());
+        LOGGER.info(service.getGamePostponed9().toString());
+        LOGGER.info(service.getNotPlaying9().toString());
+        return "addedResults";
+    }
+    
     @ExceptionHandler(Exception.class)
     @ResponseStatus(value = HttpStatus.INTERNAL_SERVER_ERROR, reason = "Error message")
     public void handleError(Exception e) {
