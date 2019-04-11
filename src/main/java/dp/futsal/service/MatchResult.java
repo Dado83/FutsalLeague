29ef2166c3560ed42a44719f71589cda3e411601
Dado83 <83deadpool@gmail.com>
2019-04-11 16:33:20 +0200

    add toString() method

--- src/main/java/dp/futsal/service/MatchResult.java
+++ src/main/java/dp/futsal/service/MatchResult.java
@@ -8,109 +8,65 @@ import java.util.logging.Logger;
 
 public class MatchResult {
 
-    private String id;
     private int matchDay;
     private String homeTeam;
     private int homeTeamID;
     private String awayTeam;
     private int awayTeamID;
-    private int goalsHome;
-    private int goalsAway;
-    List<String> pponed = new ArrayList<>();
+
+    private int goalsHome9;
+    private int goalsAway9;
+    private int goalsHome8;
+    private int goalsAway8;
+    private int goalsHome7;
+    private int goalsAway7;
+    private int goalsHome6;
+    private int goalsAway6;
+    private int goalsHome5;
+    private int goalsAway5;
 
     public MatchResult() {
 
     }
 
-    public MatchResult(int matchDay, Map<Integer, Team> teamMap, int homeTeam, int awayTeam, int goalsHome, int goalsAway,
-            Map<Integer, List<String>> postponed) {
-        Team home;
-        Team away;
-        home = teamMap.get(homeTeam);
-        away = teamMap.get(awayTeam);
+    public MatchResult(int matchDay, String homeTeam, int homeTeamID, String awayTeam, int awayTeamID,
+            int goalsHome9, int goalsAway9, int goalsHome8, int goalsAway8, int goalsHome7, int goalsAway7,
+            int goalsHome6, int goalsAway6, int goalsHome5, int goalsAway5) {
         this.matchDay = matchDay;
-        this.homeTeam = home.getTeamName();
-        this.awayTeam = away.getTeamName();
-        this.goalsHome = goalsHome;
-        this.goalsAway = goalsAway;
-        this.homeTeamID = home.getId();
-        this.awayTeamID = away.getId();
-
-        this.id = "" + matchDay + homeTeam + awayTeam;
-
-        if (this.homeTeam.equals("pauza") || this.awayTeam.equals("pauza")
-                || this.homeTeam.equals("pauza9") || this.awayTeam.equals("pauza9")
-                || this.goalsHome == -1) {
-            if (this.goalsHome == -1) {
-                pponed.add(this.homeTeam + " - " + this.awayTeam);
-                postponed.put(this.matchDay, pponed);
-                Logger.getLogger(MatchResult.class.getName()).info("utakmica nije odigrana");
-            }
-            Logger.getLogger(MatchResult.class.getName()).info("utakmica gdje ekipa pauzira");
-        } else {
-            home.setGoalsScored(home.getGoalsScored() + goalsHome);
-            home.setGoalsConceded(home.getGoalsConceded() + goalsAway);
-            away.setGoalsScored(away.getGoalsScored() + goalsAway);
-            away.setGoalsConceded(away.getGoalsConceded() + goalsHome);
-
-            home.setGamesPlayed(home.getGamesPlayed() + 1);
-            away.setGamesPlayed(away.getGamesPlayed() + 1);
-
-            if (goalsHome > goalsAway) {
-                home.setGamesWon(home.getGamesWon() + 1);
-                away.setGamesLost(away.getGamesLost() + 1);
-                home.setPoints(home.getPoints() + 3);
-            } else if (goalsHome < goalsAway) {
-                away.setGamesWon(away.getGamesWon() + 1);
-                home.setGamesLost(home.getGamesLost() + 1);
-                away.setPoints(away.getPoints() + 3);
-            } else {
-                home.setGamesDrew(home.getGamesDrew() + 1);
-                away.setGamesDrew(away.getGamesDrew() + 1);
-                home.setPoints(home.getPoints() + 1);
-                away.setPoints(away.getPoints() + 1);
-            }
-
-            home.addResults(this);
-            away.addResults(this);
-        }
-    }
-
-    @Override
-    public String toString() {
-        return matchDay + ". kolo: " + homeTeam + " " + goalsHome + "-" + goalsAway + " " + awayTeam + " ID: " + id;
-    }
-
-    public String getId() {
-        return id;
-    }
-
-    public void setId(String id) {
-        this.id = id;
+        this.homeTeam = homeTeam;
+        this.homeTeamID = homeTeamID;
+        this.awayTeam = awayTeam;
+        this.awayTeamID = awayTeamID;
+        this.goalsHome9 = goalsHome9;
+        this.goalsAway9 = goalsAway9;
+        this.goalsHome8 = goalsHome8;
+        this.goalsAway8 = goalsAway8;
+        this.goalsHome7 = goalsHome7;
+        this.goalsAway7 = goalsAway7;
+        this.goalsHome6 = goalsHome6;
+        this.goalsAway6 = goalsAway6;
+        this.goalsHome5 = goalsHome5;
+        this.goalsAway5 = goalsAway5;
     }
 
     public int getMatchDay() {
         return matchDay;
     }
 
+    @Override
+    public String toString() {
+        return "MatchResult{" + "matchDay=" + matchDay
+                + ", homeTeam=" + homeTeam + ", homeTeamID=" + homeTeamID
+                + ", awayTeam=" + awayTeam + ", awayTeamID=" + awayTeamID
+                + ", goalsHome9=" + goalsHome9 + ", goalsAway9=" + goalsAway9
+                + ", goalsHome8=" + goalsHome8 + ", goalsAway8=" + goalsAway8
+                + ", goalsHome7=" + goalsHome7 + ", goalsAway7=" + goalsAway7
+                + ", goalsHome6=" + goalsHome6 + ", goalsAway6=" + goalsAway6
+                + ", goalsHome5=" + goalsHome5 + ", goalsAway5=" + goalsAway5 + '}';
+    }
+
     public void setMatchDay(int matchDay) {
         this.matchDay = matchDay;
-    }
-
-    public int getHomeTeamID() {
-        return homeTeamID;
-    }
-
-    public void setHomeTeamID(int homeTeamID) {
-        this.homeTeamID = homeTeamID;
-    }
-
-    public int getAwayTeamID() {
-        return awayTeamID;
-    }
-
-    public void setAwayTeamID(int awayTeamID) {
-        this.awayTeamID = awayTeamID;
     }
 
     public String getHomeTeam() {
@@ -121,6 +77,14 @@ public class MatchResult {
         this.homeTeam = homeTeam;
     }
 
+    public int getHomeTeamID() {
+        return homeTeamID;
+    }
+
+    public void setHomeTeamID(int homeTeamID) {
+        this.homeTeamID = homeTeamID;
+    }
+
     public String getAwayTeam() {
         return awayTeam;
     }
@@ -129,20 +93,92 @@ public class MatchResult {
         this.awayTeam = awayTeam;
     }
 
-    public int getGoalsHome() {
-        return goalsHome;
+    public int getAwayTeamID() {
+        return awayTeamID;
     }
 
-    public void setGoalsHome(int goalsHome) {
-        this.goalsHome = goalsHome;
+    public void setAwayTeamID(int awayTeamID) {
+        this.awayTeamID = awayTeamID;
     }
 
-    public int getGoalsAway() {
-        return goalsAway;
+    public int getGoalsHome9() {
+        return goalsHome9;
     }
 
-    public void setGoalsAway(int goalsAway) {
-        this.goalsAway = goalsAway;
+    public void setGoalsHome9(int goalsHome9) {
+        this.goalsHome9 = goalsHome9;
+    }
+
+    public int getGoalsAway9() {
+        return goalsAway9;
+    }
+
+    public void setGoalsAway9(int goalsAway9) {
+        this.goalsAway9 = goalsAway9;
+    }
+
+    public int getGoalsHome8() {
+        return goalsHome8;
+    }
+
+    public void setGoalsHome8(int goalsHome8) {
+        this.goalsHome8 = goalsHome8;
+    }
+
+    public int getGoalsAway8() {
+        return goalsAway8;
+    }
+
+    public void setGoalsAway8(int goalsAway8) {
+        this.goalsAway8 = goalsAway8;
+    }
+
+    public int getGoalsHome7() {
+        return goalsHome7;
+    }
+
+    public void setGoalsHome7(int goalsHome7) {
+        this.goalsHome7 = goalsHome7;
+    }
+
+    public int getGoalsAway7() {
+        return goalsAway7;
+    }
+
+    public void setGoalsAway7(int goalsAway7) {
+        this.goalsAway7 = goalsAway7;
+    }
+
+    public int getGoalsHome6() {
+        return goalsHome6;
+    }
+
+    public void setGoalsHome6(int goalsHome6) {
+        this.goalsHome6 = goalsHome6;
+    }
+
+    public int getGoalsAway6() {
+        return goalsAway6;
+    }
+
+    public void setGoalsAway6(int goalsAway6) {
+        this.goalsAway6 = goalsAway6;
+    }
+
+    public int getGoalsHome5() {
+        return goalsHome5;
+    }
+
+    public void setGoalsHome5(int goalsHome5) {
+        this.goalsHome5 = goalsHome5;
+    }
+
+    public int getGoalsAway5() {
+        return goalsAway5;
+    }
+
+    public void setGoalsAway5(int goalsAway5) {
+        this.goalsAway5 = goalsAway5;
     }
 
 }
