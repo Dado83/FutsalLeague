a4d56e9e363a5a81d610223979edb8284a794766
Dado83 <83deadpool@gmail.com>
2019-05-14 17:27:52 +0200

    add home url test

--- src/test/java/dp/futsal/FutsalApiTests.java
+++ src/test/java/dp/futsal/FutsalApiTests.java
@@ -1,5 +1,11 @@
 package dp.futsal;
 
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.hamcrest.Matchers.containsString;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -7,7 +13,6 @@ import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMock
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
-import static org.assertj.core.api.Assertions.*;
 
 
 @RunWith(SpringRunner.class)
@@ -26,7 +31,9 @@ public class FutsalApiTests {
     }
 
     @Test
-    public void returnHome() {
-	mockMvc.perform(get("/"))
+    public void returnHome() throws Exception {
+	mockMvc.perform(get("/")).andExpect(status().isOk())
+		.andExpect(content().string(containsString("Fair Play Liga Buducih Sampiona")));
     }
+
 }
