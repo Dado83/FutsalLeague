1460a39316380c6fc77c5dd155799edfc7a54fe2
Dado83 <83deadpool@gmail.com>
2019-07-14 00:01:11 +0200

    add mDay setter&getter

--- src/main/java/dp/futsal/database/MatchPairs.java
+++ src/main/java/dp/futsal/database/MatchPairs.java
@@ -7,7 +7,6 @@ import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.Table;
 
-
 @Entity
 @Table(name = "matchpairs")
 public class MatchPairs implements Serializable {
@@ -25,55 +24,64 @@ public class MatchPairs implements Serializable {
     }
 
     public MatchPairs(int mDay, int homeTeam, int awayTeam, String gameDate, boolean isPlayed) {
-	this.mDay = mDay;
-	this.homeTeam = homeTeam;
-	this.awayTeam = awayTeam;
-	this.gameDate = gameDate;
-	this.isPlayed = isPlayed;
+        this.mDay = mDay;
+        this.homeTeam = homeTeam;
+        this.awayTeam = awayTeam;
+        this.gameDate = gameDate;
+        this.isPlayed = isPlayed;
     }
 
     public int getId() {
-	return id;
+        return id;
     }
 
     public void setId(int id) {
-	this.id = id;
+        this.id = id;
+    }
+
+    public int getmDay() {
+        return mDay;
+    }
+
+    public void setmDay(int mDay) {
+        this.mDay = mDay;
     }
 
     public int getHomeTeam() {
-	return homeTeam;
+        return homeTeam;
     }
 
     public void setHomeTeam(int homeTeam) {
-	this.homeTeam = homeTeam;
+        this.homeTeam = homeTeam;
     }
 
     public int getAwayTeam() {
-	return awayTeam;
+        return awayTeam;
     }
 
     public void setAwayTeam(int awayTeam) {
-	this.awayTeam = awayTeam;
+        this.awayTeam = awayTeam;
     }
 
     public String getGameDate() {
-	return gameDate;
+        return gameDate;
     }
 
     public void setGameDate(String gameDate) {
-	this.gameDate = gameDate;
+        this.gameDate = gameDate;
     }
 
     public boolean getIsPlayed() {
-	return isPlayed;
+        return isPlayed;
     }
 
     public void setIsPlayed(boolean isPlayed) {
-	this.isPlayed = isPlayed;
+        this.isPlayed = isPlayed;
     }
 
     @Override
     public String toString() {
-	return mDay + " " + homeTeam + " " + awayTeam;
+        return mDay + " " + homeTeam + " " + awayTeam;
     }
+
 }
