81f753df77b0875cd8b66fec2ef26b237fe93cbc
Dado83 <83deadpool@gmail.com>
2019-07-15 23:14:04 +0200

    fix Param import

--- src/main/java/dp/futsal/database/MatchPairsRepo.java
+++ src/main/java/dp/futsal/database/MatchPairsRepo.java
@@ -3,6 +3,7 @@ package dp.futsal.database;
 import java.util.List;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;;
 
 public interface MatchPairsRepo extends JpaRepository<MatchPairs, Integer> {
 
